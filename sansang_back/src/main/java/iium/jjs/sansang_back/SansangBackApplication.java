8db363a0d6dc1aec78c7d11a56e1a06e50b9d9b6
j2seon <ddj04323@bitbucket.org>
2023-10-26 17:10:14 +0900

    first commit

---
+++ sansang_back/src/main/java/iium/jjs/sansang_back/SansangBackApplication.java
@@ -0,0 +1,13 @@
+package iium.jjs.sansang_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SansangBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SansangBackApplication.class, args);
+    }
+
+}
---
+++ sansang_back/src/test/java/iium/jjs/sansang_back/SansangBackApplicationTests.java
@@ -0,0 +1,13 @@
+package iium.jjs.sansang_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansangBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
